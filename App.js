0489f76ccd335387163bb798f3e68bbbac4bf57e
joseph_ctu <sephxd1234@gmail.com>
2017-12-29 19:00:48 +0100

    refactor: Improve readability

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from "react";
+import { StyleSheet, Text, View } from "react-native";
 
-export default class App extends React.Component {
+class App extends Component {
   render() {
     return (
       <View style={styles.container}>
@@ -16,8 +16,10 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
+  }
 });
+
+export default App;
