60d7a862b5f2730a0632d8d43baf4f5f67913712
joseph_ctu <sephxd1234@gmail.com>
2017-12-29 20:50:52 +0100

    feat: changed method to getItem

--- api/index.js
+++ api/index.js
@@ -2,7 +2,7 @@ import { AsyncStorage } from "react-native";
 import { DECK_STORAGE_KEY } from "../constants";
 
 export function getDecks() {
-  AsyncStorage.getAllKeys(DECK_STORAGE_KEY)
+  AsyncStorage.getItem(DECK_STORAGE_KEY)
     .then(res => console.log("All decks", res))
     .catch(err => console.log("Error getting all decks", err));
 }
