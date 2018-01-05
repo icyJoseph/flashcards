9f4a3d8b47a1dd2b8a5ffd968c2a765122a9c449
joseph_ctu <sephxd1234@gmail.com>
2018-01-06 00:51:58 +0100

    feat: add types for actions to interact with asyncstorage

--- constants/index.js
+++ constants/index.js
@@ -3,5 +3,9 @@ export const DECK_STORAGE_KEY = "deckStorage:personal";
 
 // Action types for Redux
 export const types = {
-  GET_ALL_DECKS: "get_all_decks"
+  GET_ALL_DECKS: "get_all_decks",
+  LOAD_ALL_DECKS: "load_all_decks",
+  GET_DECK: "get_deck",
+  LOAD_DECK: "load_deck",
+  ADD_CARD: "add_card"
 };
