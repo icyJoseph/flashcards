9e03f8a9073736d394e336690e8cc554e1bd803a
joseph_ctu <sephxd1234@gmail.com>
2018-01-07 22:53:26 +0100

    fix: apply all correctly

--- sagas/index.js
+++ sagas/index.js
@@ -3,7 +3,7 @@ import { loadAllDecks } from "./loadAllDecks";
 import { types } from "../constants";
 
 function* rootSaga() {
-  yield all[takeEvery(types.GET_ALL_DECKS, loadAllDecks)];
+  yield all([takeEvery(types.GET_ALL_DECKS, loadAllDecks)]);
 }
 
 export default rootSaga;
