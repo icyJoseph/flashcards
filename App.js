381b2dcb53e4ac72e95c817ab88b10e35aea9a13
joseph_ctu <sephxd1234@gmail.com>
2018-01-07 21:28:58 +0100

    feat: wrap app with Provider

--- App.js
+++ App.js
@@ -1,14 +1,18 @@
 import React, { Component } from "react";
 import { StyleSheet, Text, View } from "react-native";
+import { Provider } from "react-redux";
+import configureStore from "./store";
 
 class App extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
+      <Provider store={configureStore()}>
+        <View style={styles.container}>
+          <Text>Open up App.js to start working on your app!</Text>
+          <Text>Changes you make will automatically reload.</Text>
+          <Text>Shake your phone to open the developer menu.</Text>
+        </View>
+      </Provider>
     );
   }
 }
