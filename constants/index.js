998b75a3bf330fc240ed00d25670f89557ae0848
joseph_ctu <sephxd1234@gmail.com>
2018-01-07 21:35:56 +0100

    feat: add fail loading all decks type

--- constants/index.js
+++ constants/index.js
@@ -5,6 +5,7 @@ export const DECK_STORAGE_KEY = "deckStorage:personal";
 export const types = {
   GET_ALL_DECKS: "get_all_decks",
   LOAD_ALL_DECKS: "load_all_decks",
+  FAILED_ALL_DECKS: "failed_all_decks",
   GET_DECK: "get_deck",
   LOAD_DECK: "load_deck",
   ADD_CARD: "add_card"
