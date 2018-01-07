27ea4cb004b6667c6f3f2241c85c1c5c3e9931f6
joseph_ctu <sephxd1234@gmail.com>
2018-01-07 22:01:15 +0100

    feat: add status bar

--- App.js
+++ App.js
@@ -1,13 +1,25 @@
 import React, { Component } from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, StatusBar } from "react-native";
 import { Provider } from "react-redux";
+import { TabNavigator, StackNavigator } from "react-navigation";
+import { Constants } from "expo";
 import configureStore from "./store";
+import { black, gray } from "./utils/colors";
+
+const FitnessStatusBar = ({ backgroundColor, ...props }) => {
+  return (
+    <View style={{ backgroundColor, height: Constants.statusBarHeight }}>
+      <StatusBar translucent backgroundColor={backgroundColor} {...props} />
+    </View>
+  );
+};
 
 class App extends Component {
   render() {
     return (
       <Provider store={configureStore()}>
-        <View style={styles.container}>
+        <View style={{ flex: 1 }}>
+          <FitnessStatusBar backgroundCOlor={black} barStyle="light-content" />
           <Text>Open up App.js to start working on your app!</Text>
           <Text>Changes you make will automatically reload.</Text>
           <Text>Shake your phone to open the developer menu.</Text>
