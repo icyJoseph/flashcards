7197ae3c5d8e1c1a634698436ce53f21d7623145
joseph_ctu <sephxd1234@gmail.com>
2018-01-07 21:14:07 +0100

    feat: implement redux store with saga middleware

---
+++ store/configureStore.js
@@ -0,0 +1,18 @@
+import { createStore, componse, applyMiddleware } from "redux";
+import rootReducer from "../reducers";
+import createSagaMiddleware from "redux-saga";
+import rootSaga from "../sagas";
+
+const sagaMiddleware = createSagaMiddleware();
+
+export default function configureStore(initialState) {
+  const store = createStore(
+    rootReducer,
+    initialState,
+    compose(applyMiddleware(sagaMiddleware))
+  );
+
+  sagaMiddleware.run(rootSaga);
+
+  return store;
+}
