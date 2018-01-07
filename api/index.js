8f36cf75b788eae2f0a49d9a1616d7fff6d96bbd
joseph_ctu <sephxd1234@gmail.com>
2018-01-07 23:14:51 +0100

    refactor: adjusted methods to return correct data

--- api/index.js
+++ api/index.js
@@ -10,20 +10,22 @@ export function getDeck(id) {
 }
 
 export function saveDeckTitle(title) {
-  return getDecks()
-    .then(res => {
-      return JSON.parse(res);
-    })
-    .then(allDecks => {
-      const newTitle = { [title]: { title, questions: [] } };
-      const newDecks = Object.assign({}, newTitle, allDecks);
-      return AsyncStorage.setItem(
-        DECK_STORAGE_KEY,
-        JSON.stringify(newDecks)
-      ).then(res => res);
-    });
+  return getDecks().then(allDecks => {
+    const newTitle = { [title]: { title, questions: [] } };
+    const newDecks = Object.assign({}, newTitle, allDecks);
+    return AsyncStorage.setItem(
+      DECK_STORAGE_KEY,
+      JSON.stringify(newDecks)
+    ).then(res => res);
+  });
 }
 
 export function addCardToDeck(title, card) {
   console.log(title, card);
+}
+
+export function removeAllDecks() {
+  return AsyncStorage.removeItem(DECK_STORAGE_KEY).then(res =>
+    console.log("REMOVED", res)
+  );
 }
