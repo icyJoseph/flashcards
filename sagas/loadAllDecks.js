4693618bc295a8f5f5a7233b7ff3749948295d4a
joseph_ctu <sephxd1234@gmail.com>
2018-01-07 22:53:47 +0100

    refactor: changed generator name

--- sagas/loadAllDecks.js
+++ sagas/loadAllDecks.js
@@ -2,7 +2,7 @@ import { call, put } from "redux-saga/effects";
 import { getDecks } from "../api";
 import { types } from "../constants";
 
-export function* loadDecks() {
+export function* loadAllDecks() {
   try {
     const decks = yield call(getDecks);
     yield put({ type: types.LOAD_ALL_DECKS, payload: decks });
