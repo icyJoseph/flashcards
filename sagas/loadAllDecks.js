9fb8f0619e1ba505f2ce4863d66b221b6b8d8d0f
joseph_ctu <sephxd1234@gmail.com>
2018-01-07 21:40:42 +0100

    feat: rootSaga listens to get events and loads

---
+++ sagas/loadAllDecks.js
@@ -0,0 +1,12 @@
+import { call, put } from "redux-saga/effects";
+import { getDecks } from "../api";
+import { types } from "../constants";
+
+export function* loadDecks() {
+  try {
+    const decks = yield call(getDecks);
+    yield put({ type: types.LOAD_ALL_DECKS, payload: decks });
+  } catch (error) {
+    yield put({ type: types.FAILED_ALL_DECKS, error });
+  }
+}
