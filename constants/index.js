b1bff88727883ac91264f5d9900f375a96d47d0e
joseph_ctu <sephxd1234@gmail.com>
2018-01-08 20:50:24 +0100

    feat: add actions for a new deck

--- constants/index.js
+++ constants/index.js
@@ -6,6 +6,10 @@ export const types = {
   GET_ALL_DECKS: "get_all_decks",
   LOAD_ALL_DECKS: "load_all_decks",
   FAILED_ALL_DECKS: "failed_all_decks",
+
+  CREATE_NEW_DECK: "create_new_deck",
+  FAILED_NEW_DECK: "failed_new_deck",
+
   GET_DECK: "get_deck",
   LOAD_DECK: "load_deck",
   FAIL_DECK: "fail_deck",
