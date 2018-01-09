e10998d3dddda014d40beefe264655811f1fefd2
joseph_ctu <sephxd1234@gmail.com>
2018-01-09 09:19:33 +0100

    feat: implement individual deck view

--- components/IndividualDeck.js
+++ components/IndividualDeck.js
@@ -4,7 +4,6 @@ import { View, Text } from "react-native";
 class IndividualDeck extends Component {
   render() {
     const { title } = this.props.navigation.state.params;
-    console.log(this.props);
     return (
       <View>
         <Text>Individual Deck</Text>
