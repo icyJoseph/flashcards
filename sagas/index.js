a173198ebc2c9dba0ff04865ed5459d4da9b7a26
joseph_ctu <sephxd1234@gmail.com>
2018-01-11 17:46:23 +0100

    feat: add watcher for addCards action

--- sagas/index.js
+++ sagas/index.js
@@ -1,11 +1,13 @@
 import { takeEvery } from "redux-saga/effects";
 import { loadAllDecks } from "./loadAllDecks";
 import { createNewDeck } from "./createNewDeck";
+import { addNewCardToDeck } from "./addCard";
 import { types } from "../constants";
 
 function* rootSaga() {
   yield takeEvery(types.GET_ALL_DECKS, loadAllDecks);
   yield takeEvery(types.CREATE_NEW_DECK, createNewDeck);
+  yield takeEvery(types.ADD_CARD, addNewCardToDeck);
 }
 
 export default rootSaga;
