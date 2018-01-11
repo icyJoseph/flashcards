493d9095a71cc90aba54864f72aeed4b4dc84e80
joseph_ctu <sephxd1234@gmail.com>
2018-01-11 17:44:44 +0100

    feat: add navigation to add card

--- components/IndividualDeck.js
+++ components/IndividualDeck.js
@@ -1,9 +1,21 @@
 import React, { Component } from "react";
-import { View, Text } from "react-native";
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  Platform
+} from "react-native";
+import { connect } from "react-redux";
+import { white, black } from "../utils/helpers";
 
 class IndividualDeck extends Component {
   _goBack = () => {
     this.props.navigation.goBack("Decks");
+  };
+
+  toAddCard = title => {
+    this.props.navigation.navigate("AddCard", { title });
   };
 
   render() {
@@ -12,9 +24,41 @@ class IndividualDeck extends Component {
       <View>
         <Text>Individual Deck</Text>
         <Text>{title}</Text>
+        <TouchableOpacity
+          style={Platform.OS === "ios" ? styles.iosBtn : styles.androidBtn}
+          onPress={() => this.toAddCard(title)}
+        >
+          <Text style={{ fontSize: 24, textAlign: "center", color: white }}>
+            Add Card
+          </Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={Platform.OS === "ios" ? styles.iosBtn : styles.androidBtn}
+        >
+          <Text style={{ fontSize: 24, textAlign: "center", color: white }}>
+            Start Quiz
+          </Text>
+        </TouchableOpacity>
       </View>
     );
   }
 }
 
-export default IndividualDeck;
+export default connect(state => ({ decks: state.decks }))(IndividualDeck);
+
+const styles = StyleSheet.create({
+  iosBtn: {
+    backgroundColor: white,
+    borderColor: black,
+    borderRadius: 3,
+    padding: 5,
+    paddingLeft: 25,
+    paddingRight: 25
+  },
+  androidBtn: {
+    margin: 5,
+    backgroundColor: black,
+    padding: 10,
+    borderRadius: 2
+  }
+});
