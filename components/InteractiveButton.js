ad06ab400fee7deaf36c78ad29db42ec64573d4b
joseph_ctu <sephxd1234@gmail.com>
2018-01-12 20:55:23 +0100

    feat: component recevies color, interaciton and text as props

--- components/InteractiveButton.js
+++ components/InteractiveButton.js
@@ -6,15 +6,36 @@ import {
   Platform,
   StyleSheet
 } from "react-native";
-import { white, black } from "../utils/colors";
 
-const InteractiveButton = ({ text, interaction }) => {
+const InteractiveButton = ({
+  text,
+  interaction,
+  primaryColor,
+  secondaryColor,
+  disable
+}) => {
   return (
     <TouchableOpacity
-      style={Platform.OS === "ios" ? styles.iosBtn : styles.androidBtn}
+      style={
+        Platform.OS === "ios"
+          ? [
+              styles.iosBtn,
+              { backgroundColor: secondaryColor, borderColor: primaryColor }
+            ]
+          : [styles.androidBtn, { backgroundColor: primaryColor }]
+      }
       onPress={interaction}
+      disabled={disable === undefined ? false : disable}
     >
-      <Text>{text}</Text>
+      <Text
+        style={{
+          fontSize: 24,
+          textAlign: "center",
+          color: Platform.OS === "ios" ? primaryColor : secondaryColor
+        }}
+      >
+        {text}
+      </Text>
     </TouchableOpacity>
   );
 };
@@ -23,8 +44,6 @@ export default InteractiveButton;
 
 const styles = StyleSheet.create({
   iosBtn: {
-    backgroundColor: white,
-    borderColor: black,
     borderRadius: 3,
     padding: 5,
     paddingLeft: 25,
@@ -32,7 +51,6 @@ const styles = StyleSheet.create({
   },
   androidBtn: {
     margin: 5,
-    backgroundColor: black,
     padding: 10,
     borderRadius: 2
   }
