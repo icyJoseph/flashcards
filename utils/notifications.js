6fc4147165a411c75da459120e155c63012e2ee7
joseph_ctu <sephxd1234@gmail.com>
2018-01-13 15:22:05 +0100

    feat: implement methods to set and remove notification

---
+++ utils/notifications.js
@@ -0,0 +1,54 @@
+import { NOTIFICATION, NOTIFICATION_KEY } from "../constants";
+import { AsyncStorage } from "react-native";
+import { Notifications, Permissions } from "expo";
+
+export const clearLocalNotification = () => {
+  return AsyncStorage.removeItem(NOTIFICATION_KEY).then(
+    Notifications.cancelAllScheduledNotificationsAsync
+  );
+};
+
+const createNotification = () => {
+  return {
+    title: "Don't forget to study",
+    body: "No pain, no gain, put sometime to study ⏲",
+    ios: {
+      sound: true,
+      priority: "high",
+      sticky: false,
+      vibrate: true
+    },
+    android: {
+      sound: true,
+      priority: "high",
+      sticky: false,
+      vibrate: true
+    }
+  };
+};
+
+export const setLocalNotification = () => {
+  AsyncStorage.getItem(NOTIFICATION_KEY)
+    .then(JSON.parse)
+    .then(data => {
+      if (data === null) {
+        Permissions.askAsync(Permissions.NOTIFICATIONS).then(({ status }) => {
+          if (status === "granted") {
+            Notifications.cancelAllScheduledNotificationsAsync();
+
+            let tomorrow = new Date();
+            tomorrow.setDate(tomorrow.getDate() + 1);
+            tomorrow.setHours(20);
+            tomorrow.setMinutes(0);
+
+            Notifications.scheduleLocalNotificationAsync(createNotification(), {
+              time: tomorrow,
+              repeat: "day"
+            });
+
+            AsyncStorage.setItem(NOTIFICATION_KEY, JSON.stringify(true));
+          }
+        });
+      }
+    });
+};
