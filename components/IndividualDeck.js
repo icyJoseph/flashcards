702e87e4c3499bc05534aa1e42450e09388bc482
joseph_ctu <sephxd1234@gmail.com>
2018-01-14 23:22:22 +0100

    feat: safeguard for undefined values, basic styling

--- components/IndividualDeck.js
+++ components/IndividualDeck.js
@@ -1,12 +1,17 @@
 import React, { Component } from "react";
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
 import { connect } from "react-redux";
 import Title from "./Title";
 import InteractiveButton from "./InteractiveButton";
 import { white, black } from "../utils/colors";
 import { capitalizer } from "../utils/helpers";
+import { getAllDecks } from "../actions";
 
 class IndividualDeck extends Component {
+  componentDidMount() {
+    this.props.dispatch(getAllDecks);
+  }
+
   _goBack = () => {
     this.props.navigation.goBack("Decks");
   };
@@ -22,31 +27,40 @@ class IndividualDeck extends Component {
   render() {
     const { title } = this.props.navigation.state.params;
     const { decks } = this.props;
-    const questions = decks[title];
-    const numberOfQuestions = questions.length;
+    const questions = decks[title] !== undefined ? decks[title].questions : [];
+    const numberOfQuestions = questions === undefined ? 0 : questions.length;
     return (
-      <View>
+      <View style={{ flex: 1 }}>
         <Title title={title} numberOfQuestions={numberOfQuestions} />
-        <InteractiveButton
-          text={"Create New Question"}
-          interaction={() => this.toAddCard(title)}
-          primaryColor={black}
-          secondaryColor={white}
-        />
-        <InteractiveButton
-          text={
-            questions && numberOfQuestions > 0
-              ? "Start Quiz"
-              : "Please Add a Question"
-          }
-          interaction={() => this.toQuiz(questions)}
-          primaryColor={black}
-          secondaryColor={white}
-          disable={questions === undefined || numberOfQuestions === 0}
-        />
+        <View style={styles.button}>
+          <InteractiveButton
+            text={"Create New Question"}
+            interaction={() => this.toAddCard(title)}
+            primaryColor={black}
+            secondaryColor={white}
+          />
+          <InteractiveButton
+            text={
+              questions && numberOfQuestions > 1
+                ? "Start Quiz"
+                : "Please Add two or more Questions"
+            }
+            interaction={() => this.toQuiz(questions)}
+            primaryColor={black}
+            secondaryColor={white}
+            disable={questions === undefined || numberOfQuestions < 1}
+          />
+        </View>
       </View>
     );
   }
 }
 
 export default connect(state => ({ decks: state.decks }), null)(IndividualDeck);
+
+const styles = StyleSheet.create({
+  button: {
+    flexDirection: "column",
+    justifyContent: "space-around"
+  }
+});
