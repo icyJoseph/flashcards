4a29ff71be0475304585a5210b939f6fda784e37
joseph_ctu <sephxd1234@gmail.com>
2018-01-15 12:28:34 +0100

    style: use Title component

--- components/PreparingQuiz.js
+++ components/PreparingQuiz.js
@@ -1,10 +1,11 @@
 import React from "react";
 import { View, Text } from "react-native";
+import Title from "./Title";
 
 const PreparingQuiz = () => {
   return (
     <View>
-      <Text>Preparing Quiz</Text>
+      <Title title={"Preparing Quiz"} />
     </View>
   );
 };
