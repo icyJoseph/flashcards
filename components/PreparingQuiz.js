c90ca90647ff77c8bafe2460a66cbf282b59497b
joseph_ctu <sephxd1234@gmail.com>
2018-01-15 10:38:05 +0100

    feat: add stand alone stateless component to display preparing quiz message

---
+++ components/PreparingQuiz.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+const PreparingQuiz = () => {
+  return (
+    <View>
+      <Text>Preparing Quiz</Text>
+    </View>
+  );
+};
+
+export default PreparingQuiz;
