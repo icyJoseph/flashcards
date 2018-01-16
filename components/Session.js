5f06a148b99a7ce82fc75b1134688799d8efd766
joseph_ctu <sephxd1234@gmail.com>
2018-01-16 12:27:27 +0100

    feat: display quiz title

--- components/Session.js
+++ components/Session.js
@@ -4,8 +4,10 @@ import DoneQuiz from "./DoneQuiz";
 import Answers from "./Answers";
 import Title from "./Title";
 import { black, white } from "../utils/colors";
+import { capitalizer } from "../utils/helpers";
 
 const Session = ({
+  title,
   quiz,
   position,
   points,
@@ -14,15 +16,22 @@ const Session = ({
   nextQuestion
 }) => {
   return (
-    <View>
-      <Title title={"QUIZ"} />
+    <View style={{ flex: 1 }}>
+      <Text style={{ alignItems: "flex-start" }}>{`Progression: ${position}/${
+        quiz.length
+      }`}</Text>
+      <View style={{ flex: 1 }}>
+        <Title title={capitalizer(title)} />
+      </View>
       {position === quiz.length ? (
-        <DoneQuiz quiz={quiz} points={points} resetQuiz={resetQuiz} />
+        <View style={{ flex: 1 }}>
+          <DoneQuiz quiz={quiz} points={points} resetQuiz={resetQuiz} />
+        </View>
       ) : (
-        <View>
-          <Title title={`Progression: ${position}/${quiz.length}`} />
-          <Title title={quiz[position].question} />
+        <View style={{ flex: 1 }}>
+          <Title title={quiz[position].question} style={{ flex: 2 }} />
           <Answers
+            style={{ flex: 4 }}
             correct={quiz[position].correct}
             incorrect={quiz[position].incorrect}
             addPoint={addPoint}
